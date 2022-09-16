6a68ac63594c8c6146ede6ae472eb32351fe8e8a
AxxlYT <azelbseva@gmail.com>
2022-09-16 15:39:41 -0400

    1.4.1

---
+++ Assets/Index_dir/DP 1 Commands.js
@@ -0,0 +1,60 @@
+window.SpeechRecognition =
+window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+recognition.interimResults = true;
+const transcript_element = document.getElementById("transcript");
+const talk_button = document.getElementById("start");
+const end_button = document.getElementById("end");
+const temp_button = document.getElementById("temp");
+
+let p = document.createElement("p");
+transcript_element.appendChild(p);
+
+recognition.addEventListener("result", (e) => {
+const transcript = Array.from(e.results)
+  .map((result) => result[0])
+  .map((result) => result.transcript)
+  .join("");
+p.textContent = transcript;
+if (e.results[0].isFinal) {
+  p = document.createElement("p");
+  p.textContent = transcript;
+  transcript_element.appendChild(p);
+  p.textContent = "";
+
+  if (transcript.includes("updates")) {
+    alert("Update Version: TEST SERVER 1.4 Stable");
+  }
+  if (transcript.includes("SRC")) {
+    window.open("https://github.com/AxxlYT/Tengu-Assistant");
+  }
+  // if (transcript.includes("close YouTube")) {
+  //   window.close("https://www.youtube.com");
+  // }
+  if (transcript.includes("open YouTube")) {
+    window.open("https://www.youtube.com");
+  }
+}
+});
+
+// function admin(){
+//     alert("Administrator Access Granted! Welcome.")
+// }
+recognition.addEventListener("end", () => {
+end_button.disabled = false;
+talk_button.disabled = true;
+});
+talk_button.addEventListener("click", () => {
+end_button.disabled = false;
+talk_button.disabled = true;
+recognition.start();
+});
+end_button.addEventListener("click", () => {
+end_button.disabled = true;
+talk_button.disabled = false;
+recognition.stop();
+});
+temp_button.addEventListener("click", () => {
+temp_button.disabled = false;
+});
---
+++ README.txt
@@ -0,0 +1,31 @@
+Hello! If you wish to use this virtual assistant, please do the following:
+
+#1 You need Visual Studio Code (To run the HTML file)
+#2 You need to install the extension on VSC- Live Server (This is to run the HTML file on a live server)
+#3 A mic (Required for the assistant to execute commands)
+#4 Chrome browser (WARNING: THIS APPLICATION WILL NOT WORK ON OPERA/OPERA GX)
+
+--Links--
+
+Visual Studio Code: https://code.visualstudio.com/
+Live Server Extension: https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer
+
+--Terms Of Usage--
+
+You shall:
+Edit the code at your discretion, Use in social media posts, share to others, etc
+
+You shall NOT:
+Redistribute as a monetized asset, not give credit to me, give the exact file to another person, use the assistant in illgal activity, 
+tweak the code for bad intentions, claim as your own/implement copyrights, etc
+
+--End--
+
+
+
+--Credits-- 
+Axxl, Project Founder And Head Coder
+DreadKnought, Bug tracker
+Zachary, Head Of Art Department And Design
+
+Links: null
